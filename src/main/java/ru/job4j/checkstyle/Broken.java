ec5e39c3c4afb0f026d18e0e47e2c5398414f370
qSivek <qsivek@gmail.com>
2023-08-15 10:23:03 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,27 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    public static final String NEW_VALUE = "";
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    private String name;
+
+    private String surname;
+
+    private int size = 10;
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+    public Broken() {
+    }
+}
