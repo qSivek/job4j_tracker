dc30c641310a9da9ec56a00b37604a32bb4976e8
qSivek <qsivek@gmail.com>
2023-08-18 15:24:21 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -14,6 +17,10 @@ public class Item {
     public Item(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return created;
     }
 
     public int getId() {
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item dateTime = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String currentDateTime = dateTime.getDateTime().format(formatter);
+        System.out.println(currentDateTime);
+    }
+}
