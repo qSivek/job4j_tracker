4390ab2cdfdf7c726b3616769f03f1a7a3542f2e
qSivek <qsivek@gmail.com>
2023-08-22 09:43:58 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,9 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item dateTime = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String currentDateTime = dateTime.getCreated().format(formatter);
-        System.out.println(currentDateTime);
+        Item dateTime = new Item("Azura", 13);
+        System.out.println(dateTime);
     }
 }
