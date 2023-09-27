5b8cf3a050922724293934b09d42fd4eb5324187
qSivek <qsivek@gmail.com>
2023-09-27 16:45:23 +0300

    4. Телефонный справочник на базе ArrayList [#41598 #448156]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -8,11 +8,14 @@ public class PhoneDictionary {
     public void add(Person person) {
         this.persons.add(person);
     }
-    
+
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.name().contains(key) || person.phone().contains(key) || person.address().contains(key) || person.surname().contains(key)) {
+            if (person.name().contains(key)
+                    || person.phone().contains(key)
+                    || person.address().contains(key)
+                    || person.surname().contains(key)) {
                 result.add(person);
             }
         }
